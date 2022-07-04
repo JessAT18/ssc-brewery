4dd07ba52ec5626cab29b82e7a38dee5d5f9502f
JessAT18 <j-aquino@outlook.com>
2022-07-04 14:16:31 -0400

    Delete beer by ID MockMVC Test

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,6 +13,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df8c39-98c4-4ae8-b663-453e8e19c311")
+                .header("Api-key", "spring")
+                .header("Api-Secret", "guru"))
+                .andExpect(status().isOk());
+    }
+
     @Test
     void findBeers() throws Exception {
         mockMvc.perform(get("/api/v1/beer"))
