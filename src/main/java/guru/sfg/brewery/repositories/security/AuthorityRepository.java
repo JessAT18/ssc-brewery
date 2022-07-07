4defba506e4f72f5e926b6b91e6d27edd5643d4a
JessAT18 <j-aquino@outlook.com>
2022-07-07 15:02:35 -0400

    Bootstrap User and Authority data, and changing project lombok from 18.12 to 18.24 for compatibility with JDK 18

--- src/main/java/guru/sfg/brewery/repositories/security/AuthorityRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/AuthorityRepository.java
@@ -3,5 +3,9 @@ package guru.sfg.brewery.repositories.security;
 import guru.sfg.brewery.domain.security.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Created by jessat18 on 7/7/22.
+ */
+
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
 }
