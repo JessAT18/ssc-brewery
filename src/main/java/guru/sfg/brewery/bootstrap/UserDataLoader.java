75880ef82aa98623d91d4dec37ef94bbc5fbf40e
JessAT18 <j-aquino@outlook.com>
2022-07-08 15:06:07 -0400

    Updating UserDataLoader with adding roles and changing MapStruct version (compatible with Lombok).

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -39,19 +40,33 @@ public class UserDataLoader implements CommandLineRunner {
     private void loadSecurityData() {
         //beer auths
         Authority createBeer = authorityRepository.save(Authority.builder().permission("beer.create").build());
-        Authority updateBeer = authorityRepository.save(Authority.builder().permission("beer.update").build());
         Authority readBeer = authorityRepository.save(Authority.builder().permission("beer.read").build());
+        Authority updateBeer = authorityRepository.save(Authority.builder().permission("beer.update").build());
         Authority deleteBeer = authorityRepository.save(Authority.builder().permission("beer.delete").build());
+
+        //customer auths
+        Authority createCustomer = authorityRepository.save(Authority.builder().permission("customer.create").build());
+        Authority readCustomer = authorityRepository.save(Authority.builder().permission("customer.read").build());
+        Authority updateCustomer = authorityRepository.save(Authority.builder().permission("customer.update").build());
+        Authority deleteCustomer = authorityRepository.save(Authority.builder().permission("customer.delete").build());
+
+        //brewery auths
+        Authority createBrewery = authorityRepository.save(Authority.builder().permission("brewery.create").build());
+        Authority readBrewery = authorityRepository.save(Authority.builder().permission("brewery.read").build());
+        Authority updateBrewery = authorityRepository.save(Authority.builder().permission("brewery.update").build());
+        Authority deleteBrewery = authorityRepository.save(Authority.builder().permission("brewery.delete").build());
 
         Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
         Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
         Role userRole = roleRepository.save(Role.builder().name("USER").build());
 
-        adminRole.setAuthorities(Set.of(createBeer, updateBeer, readBeer, deleteBeer));
+        adminRole.setAuthorities(new HashSet<>(Set.of(createBeer, updateBeer, readBeer, deleteBeer,
+                createCustomer, updateCustomer, readCustomer, deleteCustomer,
+                createBrewery, updateBrewery, readBrewery, deleteBrewery)));
 
-        customerRole.setAuthorities(Set.of(readBeer));
+        customerRole.setAuthorities(new HashSet<>(Set.of(readBeer, readCustomer, readBrewery)));
 
-        userRole.setAuthorities(Set.of(readBeer));
+        userRole.setAuthorities(new HashSet<>(Set.of(readBeer)));
 
         roleRepository.saveAll(Arrays.asList(adminRole, customerRole, userRole));
 
@@ -67,6 +82,7 @@ public class UserDataLoader implements CommandLineRunner {
                 .role(userRole)
                 .build());
 
+//        User user =
         userRepository.save(User.builder()
                 .username("scott")
                 .password(passwordEncoder.encode("tiger"))
@@ -74,5 +90,9 @@ public class UserDataLoader implements CommandLineRunner {
                 .build());
 
         log.debug("Users Loaded: " + userRepository.count());
+
+//        user.getAuthorities().forEach(authority -> {
+//            System.out.println(authority.getPermission());
+//        });
     }
 }
--- src/main/java/guru/sfg/brewery/domain/security/Role.java
+++ src/main/java/guru/sfg/brewery/domain/security/Role.java
@@ -22,7 +22,7 @@ public class Role {
     private Set<User> users;
 
     @Singular
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = {CascadeType.MERGE}, fetch = FetchType.EAGER) //, CascadeType.PERSIST
     @JoinTable(name = "role_authority",
             joinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")}
