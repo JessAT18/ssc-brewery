42b4784a6655a9592e46ea4e3867ef85224b583d
JessAT18 <j-aquino@outlook.com>
2022-07-08 15:50:08 -0400

    Assigning authorities to methods in controllers

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -28,10 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     authorize
                             .antMatchers("/h2-console/**").permitAll() //Do not use in production
                             .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**")
-                                .hasAnyRole("ADMIN", "CUSTOMER", "USER")
-                            .antMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}")
-                                .hasAnyRole("ADMIN", "CUSTOMER", "USER")
                             .mvcMatchers("/brewery/breweries")
                                 .hasAnyRole("ADMIN", "CUSTOMER")
                             .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
