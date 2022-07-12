51e6e5ef50116ebeab5a190421c128a7132d050b
JessAT18 <j-aquino@outlook.com>
2022-07-12 11:23:49 -0400

    Secure Pickup Order

--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -1,6 +1,7 @@
 package guru.sfg.brewery.web.controllers.api;
 
 import guru.sfg.brewery.security.perms.beerOrder.BeerOrderCreatePermission;
+import guru.sfg.brewery.security.perms.beerOrder.BeerOrderPickupPermission;
 import guru.sfg.brewery.security.perms.beerOrder.BeerOrderReadPermission;
 import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.web.model.BeerOrderDto;
@@ -56,6 +57,7 @@ public class BeerOrderController {
         return beerOrderService.getOrderById(customerId, orderId);
     }
 
+    @BeerOrderPickupPermission
     @PutMapping("/orders/{orderId}/pickup")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
