ef9f47005d0b400a7f8336a3304b4ffa96e19cb9
JessAT18 <j-aquino@outlook.com>
2022-07-13 11:29:00 -0400

    Setting up Persistent Token Remember Me

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -26,6 +27,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
+    private final PersistentTokenRepository persistentTokenRepository;
 
     //needed for use with Spring Data JPA SPeL
     @Bean
@@ -61,9 +63,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**", "/api/**")
                 .and()
-                .rememberMe()
-                    .key("sfg-key")
-                    .userDetailsService(userDetailsService);
+                        .rememberMe()
+                                .tokenRepository(persistentTokenRepository)
+                                .userDetailsService(userDetailsService);
+//                .rememberMe()
+//                    .key("sfg-key")
+//                    .userDetailsService(userDetailsService);
 
         //h2 console config
         http.headers().frameOptions().sameOrigin();
